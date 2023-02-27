736764a9b8514c9ea28286ff2f532784205ed039
Julien Klaus <julien.klaus@uni-jena.de>
2023-02-27 15:56:53 +0100

    main with example

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 from tec.parser import Parser
-from tec.np_backend import NumpyGenerator
+from tec.backend import NumpyGenerator
 
 
 if __name__ == "__main__":
-    term = "sum[i](log(1+exp(-y[i]+sum[j](t[j]*f[i,j]))))"
+    term = "sum[i,m,l](log(1+exp(-y[i]+sum[j](t[j,l]^2*f[i,j,m]))))"
     ag = NumpyGenerator(term)
-    ag.generate()
 
-    
+    print(ag.generate(verbose=True))
+
