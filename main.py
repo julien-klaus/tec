d2ca39e6650bb621e4355942d9bae4c13323a0c4
Julien Klaus <julien.klaus@uni-jena.de>
2023-03-07 15:52:41 +0100

    tucker decomposition example

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 
 from tec.parser import Parser
-from tec.backend import NumpyGenerator
+from tec.backend import generate
 
 
 if __name__ == "__main__":
 
-    term = "sqrt(sum[i,j,k]((A[i,j,k]-sum[n,m,l](Z[n,m,l]*B[n,i]*C[m,j]*D[k,l]))^2))"
+    expression = "sqrt(sum[i,j,k]((A[i,j,k]-sum[n,m,l](Z[n,m,l]*B[n,i]*C[m,j]*D[k,l]))^2))"
     
-    ag = NumpyGenerator(term)
-
-    print(ag.generate(verbose=True))
+    print(generate(expression, "numpy", verbose=True))
 
