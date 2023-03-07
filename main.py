00b9cf5ed6ef94599d1823623c310de8fda199e3
Julien Klaus <julien.klaus@gmx.de>
2023-03-07 15:54:28 +0100

    tucker decomposition example, no verbose

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
 
     expression = "sqrt(sum[i,j,k]((A[i,j,k]-sum[n,m,l](Z[n,m,l]*B[n,i]*C[m,j]*D[k,l]))^2))"
     
-    print(generate(expression, "numpy", verbose=True))
+    print("Generated Numpy Code:")
+    print(generate(expression, "numpy", verbose=False))
 
